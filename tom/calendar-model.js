4f12d49d8abb5e7a48072cbab26a5e6b25abca99
Tom McCluskey <bosun.tom@gmail.com>
2014-07-13 14:13:08 -0700

    cleaned up minor problems in calendar model (mostly things like semicolons missing)

--- tom/calendar-model.js
+++ tom/calendar-model.js
@@ -1,17 +1,17 @@
-var today = new Date;
+var today = new Date();
 var monthLength = function(year, month) {
   return new Date(year, month + 1, 0).getDate();
 };
 var firstDay = function(year, month) {
   return new Date(year, month, 1).getDay();
-}
+};
 var monthName = function(month) {
   return monthNames[month];
-}
+};
 var MonthView = function(date){
 
 var monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-var dayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+var dayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
   function makeTable(date) {
     var table = document.createElement('table');
@@ -22,13 +22,13 @@ var dayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'
     var monthStart = new Date(currentYear, currentMonth, 1); // holds the date for the month creation loop
 
     var getStart = function() {
-      if (monthStart.day() != 0) {
+      if (monthStart.day() !== 0) {
         monthStart.setDate(monthStart.getDate() - 1 );
         getStart();
       } else {
         return;
       }
-    }
+    };
     getStart();
 
     var makeHeader = function() {
@@ -39,7 +39,7 @@ var dayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'
         thead.appendChild(th);
       }
       table.appendChild(thead);
-    }
+    };
     makeHeader();
 
     var rowCount = function(year, month) {
@@ -50,7 +50,7 @@ var dayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'
     var height = rowCount(date.getYear(), date.getMonth());
     var tr, td;
 
-    for (var rowCount = 0; rowCount < height; rowCount++) {
+    for (var rows = 0; rows < height; rows++) {
       var row = document.createElement('tr');
       table.appendChild(row);
       var cellDateCounter = 0;
