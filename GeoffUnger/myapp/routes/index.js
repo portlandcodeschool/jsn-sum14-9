6b75bbfe55da434de71cdfffdafb203e612e333e
Geoffrey Unger <geoffunger@gmail.com>
2014-07-14 18:06:57 -0700

    Working on Index page

--- GeoffUnger/myapp/routes/index.js
+++ GeoffUnger/myapp/routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var config = require('../config.js');
+var db = require('orchestrate')(config.orchestrateKey);
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+    db.list('Categories', {"limit":"100"})
+        .then(function (result) {
+            items = result.body.results;
+            console.log(items);
+            res.render('index.hjs', {request: req.param('categoryName'), result: "Success!", categories: items});
+        })
+        .fail(function (err) {
+            res.render('index.hjs', {request: req.param('categoryName'), result: "Failure!"});
+        })
+  res.render('index', { title: "RateJoes'.com" });
 });
 
 module.exports = router;
