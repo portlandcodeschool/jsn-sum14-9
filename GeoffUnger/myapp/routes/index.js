583fec2d99c26af5077e6777b9011337feca3b1d
Geoffrey Unger <geoffunger@gmail.com>
2014-07-15 21:54:33 -0700

    Got index page category list working

--- GeoffUnger/myapp/routes/index.js
+++ GeoffUnger/myapp/routes/index.js
@@ -8,13 +8,12 @@ router.get('/', function(req, res) {
     db.list('Categories', {"limit":"100"})
         .then(function (result) {
             items = result.body.results;
-            console.log(items);
             res.render('index.hjs', {request: req.param('categoryName'), result: "Success!", categories: items});
         })
         .fail(function (err) {
             res.render('index.hjs', {request: req.param('categoryName'), result: "Failure!"});
         })
-  res.render('index', { title: "RateJoes'.com" });
+  //res.render('index', { title: "RateJoes'.com" });
 });
 
 module.exports = router;
